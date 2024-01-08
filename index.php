5443c8154ff9685ac256a2f8e1f97a1c2bc9acbd
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2024-01-08 11:39:56 +0100

    save

---
+++ index.php
@@ -0,0 +1,29 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Puissances 4</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <table border="1">
+
+        <?php for ( $j = 0; $j < 6; $j++){?>
+                <tbody>
+                    <tr>
+                        <?php for ( $i = 0; $i < 7; $i++){?>
+                        <th> <button>teste</button> </th>
+                        <?php };?>
+                    </tr>
+                </tbody>
+        <?php };?>
+    </table>
+
+</body>
+</html>
